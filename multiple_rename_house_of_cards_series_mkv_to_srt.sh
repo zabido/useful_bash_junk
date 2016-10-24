15458ee6c3f0c206a5e9c5cba79c096a58fa6cc9
zabido <zabido@gmail.com>
2016-10-24 13:08:16 +0200

    Added description and changed last line from echo to mv

--- multiple_rename_house_of_cards_series_mkv_to_srt.sh
+++ multiple_rename_house_of_cards_series_mkv_to_srt.sh
@@ -1,1 +1,2 @@
-for is in `ls | grep 720`; do j=`echo $is| awk -F'.' '{print $5}'`; nn=`ls | grep mkv | grep $j`; cnn=`sed s/mkv/srt/g <<<$nn`; echo $is "->" $cnn;  done
+## list subtitle containing 720 then split among '.' character and print the 5th into j variable as a string then create the new name that is ending with mkv and lastly change the nn string ending from mkv to str and then do the rename
+for is in `ls | grep 720`; do j=`echo $is| awk -F'.' '{print $5}'`; nn=`ls | grep mkv | grep $j`; cnn=`sed s/mkv/srt/g <<<$nn`; mv -v $is $cnn;  done
